44450030d40d2af527350573393c9f77d4e6fcf8
javilonas <admin@lonasdigital.com>
2013-08-12 05:41:50 +0200

    Revert "update files"
    This reverts commit a32f0771f9e8b1f2c913131dd6c979b8250b11ae.

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
 /* This file is auto generated, version Stock */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#Stock SMP PREEMPT Sun Aug 11 19:00:21 CEST 2013"
+#define UTS_VERSION "#Stock SMP PREEMPT Sun Aug 11 14:38:58 CEST 2013"
 #define LINUX_COMPILE_BY "lonas"
 #define LINUX_COMPILE_HOST "lonas-linux"
 #define LINUX_COMPILER "gcc version 4.7 (GCC) "
